e5bb6ac25684cb9c9b8815d3752927c24d273f90
calinursaciuc <calin.ursaciuc@digital.hmrc.gov.uk>
2017-11-20 13:19:20 +0000

    VOAC-90: WIP

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -25,7 +25,7 @@ object MicroServiceBuild extends Build with MicroService {
   private val playReactivemongoVersion = "5.2.0"
   private val playConditionalFormMappingVersion = "0.2.0"
   private val playLanguageVersion = "3.4.0"
-  private val bootstrapVersion = "0.13.0"
+  private val bootstrapVersion = "1.0.0"
   private val reactiveMongoVersion = "6.1.0"
   private val swaggerVersion = "0.6.2"
 
