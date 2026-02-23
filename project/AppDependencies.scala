bd99326c0bd9ade89217f04fb3e30b6a133a2f48
Yuriy Tumakha <tumakha@gmail.com>
2026-02-23 12:26:13 +0000

    VOA-3634 Contact Forms: Addressed vulnerability CVE-2025-48924 by upgrading bootstrap-play

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,16 +3,13 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "10.5.0"
+  private val bootstrapVersion = "10.6.0"
   private val hmrcMongoVersion = "2.12.0"
 
   // Test dependencies
-  private val scalaTestPlusPlayVersion = "7.0.2"
-  private val scalaTestVersion         = "3.2.19"
-  private val scalacheckVersion        = "3.2.19.0"
-  private val mockitoVersion           = "3.2.19.0"
-  private val scalaGuiceVersion        = "6.0.0" // Use 6.0.0 because 7.0.0 is not compatible with play-guice:3.0.10
-  private val flexMarkVersion          = "0.64.8"
+  private val scalacheckVersion = "3.2.19.0"
+  private val mockitoVersion    = "3.2.19.0"
+  private val scalaGuiceVersion = "6.0.0" // Use 6.0.0 because 7.0.0 is not compatible with play-guice:3.0.10
 
   private val compile = Seq(
     "uk.gov.hmrc"       %% "bootstrap-backend-play-30" % bootstrapVersion,
@@ -20,10 +17,7 @@ object AppDependencies {
   )
 
   private val commonTests = Seq(
-    "org.scalatestplus.play" %% "scalatestplus-play" % scalaTestPlusPlayVersion % Test,
-    "org.playframework"      %% "play-test"          % PlayVersion.current      % Test,
-    "org.scalatest"          %% "scalatest"          % scalaTestVersion         % Test,
-    "com.vladsch.flexmark"    % "flexmark-all"       % flexMarkVersion          % Test // for scalatest 3.2.x
+    "uk.gov.hmrc" %% "bootstrap-test-play-30" % bootstrapVersion % Test
   )
 
   private val testOnly = Seq(
